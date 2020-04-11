6ace97c39f217e29d65f3d3084d92a6cbcfb54a9
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-11 04:53:39 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+ @@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+ 
+      item_name = req.path.split("/items/").last
+      name = @@items.find{|s| s.name == item_name}
+        if name !=nil 
+         resp.write name.price
+        else
+           resp.write "Item not found"
+           resp.status = 400
+         end
+      else
+        
+        resp.status = 404
+        resp.write "Route not found"
+       end
+    resp.finish
+  end
+end
